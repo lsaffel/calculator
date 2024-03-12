444fb83f320535799e61afb47bbc5363f0033b12
Lisa <llvvss@icloud.com>
2024-03-11 19:00:30 -0600

    case statement, calculator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# calculator
+calculator
+
+case statement
+-, +, /, \*, % (modulo), default - for an unexpected or 'wrong' character
+Must have the break; statement after each case,  
+otherwise, the program will keep executing the case code  
+until it hits a break statement.
--- Source Files/calculator.cpp
+++ Source Files/calculator.cpp
@@ -3,6 +3,39 @@ using namespace std;
 
 int main()
 {
+    float num1, num2;
+    char operation;
+    cout << "Calculator" << endl;
+    cin >> num1 >> operation >> num2;
 
-    
+    switch (operation)
+    {
+        case '-': cout << num1 << operation << num2 << "=" << num1 - num2; break;
+
+        case '+': cout << num1 << operation << num2 << "=" << num1 + num2; break;
+
+        case '/': cout << num1 << operation << num2 << "=" << num1 / num2; break;
+
+        case '*': cout << num1 << operation << num2 << "=" << num1 * num2; break;
+
+        // case '%': cout << num1  << operation << num2 << "=" << num1 % num2; break;
+        // that one gives an error "expression must have integral or unscoped enum type"
+        // We have to make sure that in this case, it's two integers, not float
+
+        case '%':
+            bool isNum1Int, isNum2Int;
+            isNum1Int = ((int)num1 == num1);   // 5 == 5.0 is true   
+            // the first 5 had a chopped off decimal part. 
+            // The user entered 5.0
+            isNum2Int = ((int)num2 == num2);
+
+            if (isNum1Int && isNum2Int)
+                cout << num1 << operation << num2 << "=" << (int)num1 % (int)num2;
+            else
+                cout << "This operation is not valid!";
+            break;
+        default: cout << "Not valid operation!" << endl;
+    }
+
+
 }
